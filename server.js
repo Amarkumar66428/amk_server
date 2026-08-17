9167a21c786cd7c89c033cc47b752c07ddcfef65
Amarkumar66428 <57205407+Amarkumar66428@users.noreply.github.com>
2026-08-17 12:12:07 +0530

    puppeter fixes

--- server.js
+++ server.js
@@ -8,7 +8,13 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 connectDB();
-app.use(cors());
+app.use(cors(
+  {
+    origin: { origin: process.env.CLIENT_URL || 'http://localhost:3000' },
+    credentials: true
+  }
+));
+
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
